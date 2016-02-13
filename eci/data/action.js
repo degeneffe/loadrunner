9b2a5dc888eea7b99434179f972d184108e1295d
degeneffe <degeneffe@gmail.com>
2016-02-13 21:17:00 +0100

    Revert "Revert "v0.2""
    This reverts commit 1cfcf587b4d2fcdfc8691fb97c02e6e4a1d7be91.

--- eci/data/action.js
+++ eci/data/action.js
@@ -1,37 +1,12 @@
 //"'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 //" Script Title       : 
 //"                      
-//" Script Date        : Sat Feb 13 20:55:31 2016
+//" Script Date        : Sat Feb 13 21:00:41 2016
 //"                       
 //"'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
 function Action()
 {
-	web.url(
-		{
-			name : 'www.elcorteingles.es', 
-			url : 'http://www.elcorteingles.es/', 
-			targetFrame : '', 
-			resource : 0, 
-			recContentType : 'text/html', 
-			referer : '', 
-			snapshot : 't1.inf', 
-			mode : 'HTML', 
-			extraRes :  [
-				{url : '/sgfm/SGFM/assets/stylesheets/fonts/opensans-regular-webfont.woff', referer : 'http://www.elcorteingles.es/sgfm/SGFM/assets/stylesheets/crs.css'},
-				{url : '/sgfm/SGFM/assets/stylesheets/fonts/opensans-bold-webfont.woff', referer : 'http://www.elcorteingles.es/sgfm/SGFM/assets/stylesheets/crs.css'},
-				{url : '/sgfm/SGFM/assets/stylesheets/fonts/moonshine-font.woff', referer : 'http://www.elcorteingles.es/sgfm/SGFM/assets/stylesheets/crs.css'},
-				{url : '/sgfm/SGFM/assets/javascripts/library/external/dust-helpers.min.js'},
-				{url : '/sgfm/SGFM/assets/stylesheets/fonts/lato-regular-webfont.woff', referer : 'http://www.elcorteingles.es/sgfm/SGFM/assets/stylesheets/crs.css'},
-				{url : '/sgfm/SGFM/assets/javascripts/library/external/dust-core.min.js'},
-				{url : 'http://www.googletagmanager.com/gtm.js?id=GTM-TMQVMD'},
-				{url : '/sgfm/SGFM/assets/javascripts/compiled/templates.js'},
-				{url : '/sgfm/SGFM/assets/javascripts/library/external/form2js.min.js'},
-				{url : '/sgfm/SGFM/assets/stylesheets/images/confianza-online-icon.svg', referer : 'http://www.elcorteingles.es/sgfm/SGFM/assets/stylesheets/crs.css'}
-			]
-		}
-	);
-
 	return 0;
 }
 
